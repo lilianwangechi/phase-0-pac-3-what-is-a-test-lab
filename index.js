f3a45a1da3f8ad68f1b0f769af6a0ddc7615bf21
lilian <lilian.wangechi@student.moringaschool.com>
2022-07-30 23:07:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";//changed the name from Joe to Susan then run npm test
+const height = 39;//chnage the height from 74 to 39 then run npm test
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
